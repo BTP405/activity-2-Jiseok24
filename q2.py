06ab8e5b6a4ca04f9eebc7dc4de0869465b6a886
JISEOK SHIM <96095050+JiseokS@users.noreply.github.com>
2024-02-14 12:06:25 -0500

    initial commit

--- q2.py
+++ q2.py
@@ -1,1 +1,30 @@
-def graphSnowfall(t):
+# Write a Python function graphSnowfall(t) which retrives data in a text file t and displays that information in a bar chart.
+
+# function grahSnowFall()
+def graphSnowFall(t):
+
+    counter = [0, 0, 0, 0, 0]
+
+    file = open("t.txt", "r")
+    for line in file:
+        snow = int(line.strip())
+        if snow < 11:
+            counter[0] += 1
+        elif snow < 21:
+            counter[1] += 1
+        elif snow < 31:
+            counter[2] += 1
+        elif snow < 41:
+            counter[3] += 1
+        else:
+            counter[4] += 1
+    file.close()
+
+    print(counter[0], "between 0-10cms")
+    print(counter[1], "between 0-10cms")
+    print(counter[2], "between 0-10cms")
+    print(counter[3], "between 0-10cms")
+    print(counter[4], "between 0-10cms")
+
+
+graphSnowFall("t.txt")
--- q3.py
+++ q3.py
@@ -1,1 +1,20 @@
+# Write a Python function wordCount(t) which retrives data in a text file t and returns a dictionary where the keys are unique words in the files
+# and the corresponding values are lists of line numbers where the words are found in the text.
+
+# function wordCount()
 def wordCount(t):
+    dictionary = {}
+    file = open(t, 'r')
+    for line in file:
+        words = line.split()
+        for word in words:
+            # Remove punctuation marks and convert to lowercase
+            word = word.strip().lower()
+            if word not in dictionary:
+                dictionary[word] = 1
+            else:
+                dictionary[word] += 1
+    return dictionary
+
+filename = "sample.txt" 
+print(wordCount("sample.txt"))
