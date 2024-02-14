b3179687223cff5f264f12f4a14ffed53fe6fd9d
JISEOK SHIM <96095050+JiseokS@users.noreply.github.com>
2024-02-14 12:03:05 -0500

    initial commit

--- q1.py
+++ q1.py
@@ -1,1 +1,15 @@
-def getPrimeNumbers(n):
+# Helper function - determine particular number is prime
+def prime(num):
+    if num < 2:
+        return False
+    for i in range (2, int(num**0.5) +1):
+        if num % i == 0:
+            return False
+    return True
+
+# getPrimeNumber funciton
+def getPrimeNumber(n):
+    return [x for x in range (2, n + 1) if prime(x)]
+
+
+print(getPrimeNumber(20))
